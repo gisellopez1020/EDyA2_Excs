a40b6cea1ba9959486262959584baab26702a1e0
KAREN GISEL LOPEZ ORDONEZ <karen_gisel.lopez@uao.edu.co>
2024-10-02 16:24:18 -0500

    Parcial2: Calculadora en React

---
+++ calculator/src/App.jsx
@@ -0,0 +1,14 @@
+import Calculadora from './components/calculadora'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Calculadora</h1>
+      <Calculadora />
+    </div>
+  )
+}
+
+export default App
