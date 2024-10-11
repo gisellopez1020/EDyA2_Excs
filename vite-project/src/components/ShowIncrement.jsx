cccb092df7e1e1b1cfac95de6dac1cf508e5897e
KAREN GISEL LOPEZ ORDONEZ <karen_gisel.lopez@uao.edu.co>
2024-10-11 16:19:23 -0500

    Hechos Challenges hasta el número 7

---
+++ vite-project/src/components/ShowIncrement.jsx
@@ -0,0 +1,10 @@
+export const ShowIncrement = ({incrementFather}) => {
+
+    return (
+        <button
+            onClick={() => incrementFather( 5 )}
+        >
+            Increment
+        </button>
+    )
+}
