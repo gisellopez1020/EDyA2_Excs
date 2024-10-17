f2fd009cfb51573072255eea60dd8dae394871bc
KAREN GISEL LOPEZ <karenglopez1020@gmail.com>
2024-10-16 22:30:36 -0500

    Challenges de Reducer hechos

--- vite-project/src/components/CallbackHook.jsx
+++ vite-project/src/components/CallbackHook.jsx
@@ -1,22 +1,19 @@
 import { useState, useCallback } from "react";
-import { ShowIncrement} from './ShowIncrement'
+import { ShowIncrement } from "./ShowIncrement";
 
-export const CallbackHook = () =>{
-    const [counter, setCounter] = useState(10)
+export const CallbackHook = () => {
+  const [counter, setCounter] = useState(10);
 
-    const incrementFather = useCallback(
-        () => {
-            console.log('calling setcounter')
-            setCounter( (value) => value +1)
-        },
-        [],
-    )
+  const incrementFather = useCallback((c) => {
+    console.log("calling setcounter", c);
+    setCounter((value) => value + c);
+  }, []);
 
-    return (
-        <>
-            <div>CallbackHook {counter}</div>
-
-            <ShowIncrement incrementFather={incrementFather} />        
-        </>
-    )
-}
+  return (
+    <>
+      <div>CallbackHook {counter}</div>
+      <ShowIncrement incrementFather={incrementFather} />
+           
+    </>
+  );
+};
